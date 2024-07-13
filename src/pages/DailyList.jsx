bed443a04326917e4a5afad05225cb670d268d8f
lucy726j <lucy726jeon@gmail.com>
2024-07-13 20:07:58 +0900

    dailyList 페이지

--- src/pages/DailyList.jsx
+++ src/pages/DailyList.jsx
@@ -1,5 +1,22 @@
 import Layout from '../components/Layout'
+import styles from '../styles/DailyList.module.css'
+import folderImg from '../assets/images/folder.png'
+import DailyBox from '../components/DailyBox'
 
 export default function DailyList() {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <div className={styles.container}>
+        <div className={styles.title}>데일리</div>
+        <div className={styles.boxContainer}>
+          <DailyBox />
+          <DailyBox />
+          <DailyBox />
+          <DailyBox />
+          <DailyBox />
+
+        </div>
+      </div>
+    </Layout>
+  )
 }
