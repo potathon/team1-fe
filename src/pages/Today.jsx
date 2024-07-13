c57920e27080757a5d23a1c1f7c9613217fa90b0
erica0321 <hyunahrho29@gmail.com>
2024-07-14 01:20:27 +0900

    refactor: Today pages

--- src/pages/Today.jsx
+++ src/pages/Today.jsx
@@ -1,23 +1,36 @@
+import React, { useState, useEffect } from 'react'
 import Layout from '../components/Layout'
 import styles from '../styles/Today.module.css'
 import folder from '../assets/images/folder.png'
 import voidImg from '../assets/images/voidImg.png'
 import Warning from '../components/Warning'
-import TodayQuestion from '../components/TodayQuestion'
-import { useState, useEffect } from 'react'
+import TodayQuestionList from '../components/TodayQuestionList'
 import TodayAnswer from '../components/TodayAnswer'
 import { useParams } from 'react-router-dom'
+import { formatDate } from '../utils/transformDate'
 
 export default function Today() {
   const [isEnd, setIsEnd] = useState(false)
-  const [answer1, setAnswer1] = useState('')
-  const [answer2, setAnswer2] = useState('')
-  const [answer3, setAnswer3] = useState('')
+  const [answers, setAnswers] = useState(['', '', ''])
+  const [data, setData] = useState([])
+
+  const { id } = useParams()
 
   useEffect(() => {
-    console.log(isEnd)
-    console.log(answer1[0]?.text, answer2[0]?.text, answer3[0]?.text)
-  }, [isEnd, answer1, answer2, answer3])
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`https://goldenteam.site/daily/test/${id}`)
+        if (response.ok) {
+          const responseData = await response.json()
+          setData(responseData)
+        }
+      } catch (err) {
+        console.log('Error fetching data', err)
+      }
+    }
+
+    fetchData()
+  }, [id])
 
   return (
     <Layout>
@@ -26,16 +39,17 @@ export default function Today() {
         <div className={styles.container}>
           <div className={styles.date}>
             <img alt='folder' src={folder} className={styles.folder} />
-            <div className={styles.date}>7월 13일 (토)</div>
+            <div className={styles.date}>
+              {data.length > 0 ? formatDate(data[0].date) : ''}
+            </div>
           </div>
           <div className={styles.mainBottom}>
             <div className={styles.left}>
               <TodayQuestionList
-                isEnd={setIsEnd}
+                data={data}
+                isEnd={isEnd}
                 setIsEnd={setIsEnd}
-                setAnswer1={setAnswer1}
-                setAnswer2={setAnswer2}
-                setAnswer3={setAnswer3}
+                setAnswers={setAnswers}
               />
             </div>
             <div className={styles.right}>
@@ -46,9 +60,9 @@ export default function Today() {
         <hr className={styles.line} />
         {isEnd ? (
           <TodayAnswer
-            answer1={answer1[0]?.text}
-            answer2={answer2[0]?.text}
-            answer3={answer3[0]?.text}
+            answer1={answers[0]}
+            answer2={answers[1]}
+            answer3={answers[2]}
           />
         ) : (
           <Warning />
@@ -56,61 +70,4 @@ export default function Today() {
       </div>
     </Layout>
   )
-}
-
-function TodayQuestionList({
-  isEnd,
-  setIsEnd,
-  setAnswer1,
-  setAnswer2,
-  setAnswer3,
-}) {
-  const [questionNum, setQuestionNumber] = useState(1)
-  const [data, setData] = useState([])
-
-  const id = useParams().id
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`https://goldenteam.site/daily/test/${id}`)
-
-        if (response.ok) {
-          const responseData = await response.json()
-          setData(responseData)
-        }
-      } catch (err) {
-        console.log('Error fetch data', err)
-      }
-    }
-
-    fetchData()
-  }, [id])
-
-  return data ? (
-    <div className={styles.questions}>
-      <TodayQuestion
-        setQuestionNumber={setQuestionNumber}
-        setAnswer={setAnswer1}
-        question={data[0].question}
-        questionNumber={questionNum}
-        number={1}
-      />
-      <TodayQuestion
-        setAnswer={setAnswer2}
-        setQuestionNumber={setQuestionNumber}
-        question={data[1].question}
-        questionNumber={questionNum}
-        number={2}
-      />
-      <TodayQuestion
-        setAnswer={setAnswer3}
-        questionNumber={questionNum}
-        question={data[2].question}
-        isEnd={isEnd}
-        setIsEnd={setIsEnd}
-        number={3}
-      />
-    </div>
-  ) : null
 }
