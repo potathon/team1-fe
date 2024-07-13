50d581bb261559c5fe1bd0a074e6876a2b6a053c
lucy726j <lucy726jeon@gmail.com>
2024-07-13 17:18:50 +0900

    void 사진 최최종

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
 import Layout from '../components/Layout'
+import potatoRoadImg from '../assets/images/potatoRoad.png'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return <Layout></Layout>
+  return (
+    <Layout>
+      <div className={styles.mainImg}>
+        <img
+          src={potatoRoadImg}
+          alt='potatoRoad'
+          className={styles.potatoRoadImg}
+        />
+      </div>
+      <button>튀김기 입장</button>
+    </Layout>
+  )
 }
