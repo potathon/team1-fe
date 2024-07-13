679f58d3e4c6ded097df4f36b6e23bdb764e6217
lucy726j <lucy726jeon@gmail.com>
2024-07-13 17:52:48 +0900

    startBnt에 클릭 이벤트 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import Layout from '../components/Layout'
 import potatoRoadImg from '../assets/images/potatoRoad.png'
 import styles from '../styles/Home.module.css'
-import DailyList from './DailyList'
+import { useNavigate } from 'react-router-dom'
 
 export default function Home() {
+  const navigator = useNavigate()
+  const handleButtonClick = () => {
+    navigator('/dailyList')
+  }
   return (
     <Layout>
       <div className={styles.container}>
@@ -14,7 +18,7 @@ export default function Home() {
             className={styles.potatoRoadImg}
           />
         </div>
-        <button className={styles.startBnt} onClick={<DailyList />}>
+        <button className={styles.startBnt} onClick={handleButtonClick}>
           튀김기 입장
         </button>
       </div>
